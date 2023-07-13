7de361d43d9185a5bdc547b79e4c2e4bad833d40
singh123456priyanka <singh123456priyanka22@gmail.com>
2023-07-13 11:38:58 +0530

    1stCommit

---
+++ src/test/java/testJenkins/TestJenkins.java
@@ -0,0 +1,12 @@
+package testJenkins;
+
+import org.testng.annotations.Test;
+
+public class TestJenkins {
+	@Test
+	public void m() {
+		
+		System.out.println("Hello I m Priyanka Singh");
+	}
+
+}
